4f270f7fc6fa13898c6c75e1b0c4b7d2a50af8d6
Vallery Lancey <vallerylancey@gmail.com>
2019-01-25 00:25:50 -0800

    Switched scheduler import from k/k/staging API to main (fixes compile failure)

--- pkg/scheduler/core/generic_scheduler.go
+++ pkg/scheduler/core/generic_scheduler.go
@@ -29,7 +29,7 @@ import (
 	"k8s.io/klog"
 
 	"k8s.io/api/core/v1"
-    policy "k8s.io/api/policy/v1beta1"
+	policy "k8s.io/api/policy/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/errors"
