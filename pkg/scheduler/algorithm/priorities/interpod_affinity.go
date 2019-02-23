27d56174d3ea364d9808b637645f675d43b07200
wangqingcan <wangqingcan@baidu.com>
2019-02-23 21:07:50 +0800

    3

--- pkg/scheduler/algorithm/priorities/interpod_affinity.go
+++ pkg/scheduler/algorithm/priorities/interpod_affinity.go
@@ -212,7 +212,7 @@ func (ipa *InterPodAffinity) CalculateInterPodAffinityPriority(pod *v1.Pod, node
 			}
 		}
 	}
-	workqueue.ParallelizeUntil(context.TODO(), runtime.NumCPU()*2, len(allNodeNames), processNode)
+	workqueue.ParallelizeUntil(context.TODO(), runtime.NumCPU()*3, len(allNodeNames), processNode)
 	if pm.firstError != nil {
 		return nil, pm.firstError
 	}
